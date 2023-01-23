11c23a355df48937605305599a5d3e20dbd7bd3f
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-24 01:06:40 +0430

    react router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from "./pages/Home/Home"
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Welcome to HerayNet
-      </header>
+      
+      <Router>
+        <Navbar /> 
+        <Routes>
+          <Route path='/' element = {<Home />} />
+          <Route path='*' element = {<h2>Not Found</h2>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
