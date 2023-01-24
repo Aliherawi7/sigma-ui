4526c8be695f8b3e5a3dd8c77af1b04ae51d1de5
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-24 23:11:42 +0430

    React router and layout contanier added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home/Home"
 import Navbar from './components/navbar/Navbar';
+import SearchBar from './components/searchbar/SearchBar';
 
 function App() {
   return (
     <div className="App">
-      
-      <Router>
-        <Navbar /> 
-        <Routes>
-          <Route path='/' element = {<Home />} />
-          <Route path='*' element = {<h2>Not Found</h2>} />
-        </Routes>
-      </Router>
+      <main className='layout'>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='*' element={<h2>Not Found</h2>} />
+          </Routes>
+        </Router>
+      </main>
     </div>
   );
 }
