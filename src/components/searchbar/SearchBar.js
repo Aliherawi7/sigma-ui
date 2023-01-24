b1f6c92adc54b4e76806052a899c4e5dba46c360
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-24 23:17:39 +0430

    search bar componet idea changed

--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import "./SearchBar.css"
+
 
 function SearchBar() {
   return (
-    <header>Brand logo and SearchBar</header>
+    <header className='main_header'>
+      
+      <section className='search_bar'>
+        <form>
+          <input type="text" className='search_box' />
+        </form>
+      </section>
+    </header>
   )
 }
 
