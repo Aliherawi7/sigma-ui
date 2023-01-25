3dbe436e2055fc63e1d0c3d45daca97f1cb2fa0f
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-25 23:11:17 +0430

    NotificationArea component added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home/Home"
 import Navbar from './components/navbar/Navbar';
-import SearchBar from './components/searchbar/SearchBar';
+import NotificationArea from './components/NotificationArea/NotificationArea';
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <main className='layout'>
         <Router>
           <Navbar />
+          <NotificationArea />
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='*' element={<h2>Not Found</h2>} />
