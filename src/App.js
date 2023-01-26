978cc37540a7dee1d8042551739aafdf8ea8548f
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-26 21:56:21 +0430

    react lazy loading feature added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from "./pages/Home/Home"
-import Navbar from './components/navbar/Navbar';
-import NotificationArea from './components/NotificationArea/NotificationArea';
+import Spinner from './components/UI/Loading/Spinner';
+import { Suspense } from 'react';
+
+const Home = React.lazy(() => import("./pages/Home/Home"))
+const Navbar = React.lazy(() => import('./components/navbar/Navbar'))
+const NotificationArea = React.lazy(() => import('./components/NotificationArea/NotificationArea'))
+const Chat = React.lazy(() => import('./pages/Chat/Chat'))
 
 function App() {
   return (
-    <div className="App">
-      <main className='layout'>
-        <Router>
-          <Navbar />
-          <NotificationArea />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='*' element={<h2>Not Found</h2>} />
-          </Routes>
-        </Router>
-      </main>
-    </div>
+      <div className="App">
+        <main className='layout'>
+          <Router>
+          <Suspense fallback={<Spinner />}>
+            <Navbar />
+            <NotificationArea />
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/chat' element={<Chat />} />
+              <Route path='load' element={<Spinner />} />
+              <Route path='*' element={<h2>Not Found</h2>} />
+            </Routes>
+            </Suspense>
+          </Router>
+        </main>
+      </div>
   );
 }
 
