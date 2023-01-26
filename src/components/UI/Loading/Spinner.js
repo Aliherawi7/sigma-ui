697b020c061035b8a49ce947fadd80878e207e9e
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-26 22:03:47 +0430

    main loading animation added

---
+++ src/components/UI/Loading/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Spinner.css"
+
+function Spinner() {
+  return (
+    <div class="spinner__wrapper">
+        <div class="spinner"></div>
+    </div>
+  )
+}
+
+export default Spinner
