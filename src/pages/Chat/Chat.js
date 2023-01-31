6f509758abfc0a769f466dd506f2945bd8cce2a3
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-31 21:41:35 +0430

    chat component elements are adding

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from '../../components/UI/button/Button'
 import { Icons } from '../../constants/UiConstant'
 import "./Chat.css"
 
@@ -65,7 +66,40 @@ function Chat() {
                 </div>
             </div>
             <div className='messages_panel'>
+                <div className='header display_flex'>
+                    <img src='\images\ProfileImages\user2.jpg' className='profile_avatar_small' />
+                    <div className='contact_name_status'>
+                        <h5 className='name'>
+                            Maria Johns
+                            <span className='active_badge'> </span>
+                        </h5>
+                        <p className='last_seen'>Active now</p>
+                    </div>
 
+                </div>
+                <div className='message_panel'>
+                    <div className='messages_container display_flex flex_direction_column'>
+                        <div className='send_message display_flex'>
+                            <div className='message_body display_flex flex_direction_column'>
+                                <p className='message_value'>Hi guys this is a test message</p>
+                                <span className='message_date'>10:20 am</span>
+                            </div>
+                            <img src='\images\ProfileImages\user3.jpg' className='profile_avatar_small' />
+                        </div>
+                        <div className='receive_message display_flex'>
+                            <img src='\images\ProfileImages\user2.jpg' className='profile_avatar_small' />
+                            <div className='message_body display_flex flex_direction_column'>
+                                <p className='message_value'>Hi guys this is a test message I made this line's width a little longer to test the size of message body</p>
+                                <span className='message_date'>10:20 am</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='input_panel'>
+                        <Button icon={Icons.emojis.smile} />
+                        <input type={'text'} className='input' placeholder='Type a message'/>
+                        <Button icon={Icons.send} />
+                    </div>
+                </div>
             </div>
         </section>
     )
