c06bd005aeb187bd54296ed6a6ec8fc12fdb32be
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-31 23:50:07 +0430

    Button component type changed to button

--- src/components/UI/button/Button.js
+++ src/components/UI/button/Button.js
@@ -3,7 +3,7 @@ import "./Button.css"
 
 function Button({name, type, icon, click}) {
   return (
-    <button className={`btn ${type}`} onclick={click}><i className={icon}></i> {name}</button>
+    <button className={`btn ${type}`} onclick={click} type="button"><i className={icon}></i> {name}</button>
   )
 }
 
