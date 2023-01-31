24521db1addd43bb815e4396188a9f663f122f6b
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-31 21:38:44 +0430

    onclick property added to the Button component

--- src/components/UI/button/Button.js
+++ src/components/UI/button/Button.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import "./Button.css"
 
-function Button({name, type, icon}) {
+function Button({name, type, icon, click}) {
   return (
-    <button className={`btn ${type}`}><i className={icon}></i> {name}</button>
+    <button className={`btn ${type}`} onclick={click}><i className={icon}></i> {name}</button>
   )
 }
 
