1b3d8b88bc9ed0a63810f98ba88459295f5f91e6
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-31 23:49:36 +0430

    login page added to the routes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const Home = React.lazy(() => import("./pages/Home/Home"))
 const Navbar = React.lazy(() => import('./components/navbar/Navbar'))
 const NotificationArea = React.lazy(() => import('./components/NotificationArea/NotificationArea'))
 const Chat = React.lazy(() => import('./pages/Chat/Chat'))
+const Login = React.lazy(() => import("./pages/Login/Login"))
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
               <Route path='/' element={<Home />} />
               <Route path='/chat' element={<Chat />} />
               <Route path='load' element={<Spinner />} />
+              <Route path='/login' element={<Login />} />
               <Route path='*' element={<h2>Not Found</h2>} />
             </Routes>
             </Suspense>
