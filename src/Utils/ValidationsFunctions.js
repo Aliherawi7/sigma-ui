f0330d0b5dd8117c3373660e5e9c58e972b2174f
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-13 15:21:37 -0800

    all validation functions updated

--- src/Utils/ValidationsFunctions.js
+++ src/Utils/ValidationsFunctions.js
@@ -1,18 +1,18 @@
 
 export const validationFuncations = {
     nameValidation: (name) => {
-        if(name.length > 2){
+        if (name.length > 2) {
             return [true, ""]
-        }else{
+        } else {
             return [false, "invalid name"]
         }
     },
-    emailValidation: (email) =>{
+    emailValidation: (email) => {
         // don't forget to find a better regex
         let pattern = /([a-z0-9_-]+)@([\da-z-]+)([a-z\.]{2,6})/ig;
-        if(pattern.test(email)){
+        if (pattern.test(email)) {
             return [true, ""]
-        }else {
+        } else {
             return [false, "invalid email"]
         }
     },
@@ -20,25 +20,36 @@ export const validationFuncations = {
         const dateObject = new Date(date);
         const currentDate = new Date();
         let isValid = currentDate.getFullYear() - dateObject.getFullYear() >= 18 ? true : false;
-        if(isValid){
+        if (isValid) {
             return [true, ""]
-        }else{
+        } else {
             return [false, "the age should be 18 or higher"]
         }
     },
-    passwordValidation : (password)=>{
-        const isValid = password.length > 5 ? true : false
-        if(isValid){
+    genderValidation: (value) => {
+        value = value.toLowerCase()
+        if (value == "male" || value == "female") {
             return [true, ""]
-        }else{
+        } else {
+            return [false, "select the gender"]
+        }
+    },
+
+
+
+    passwordValidation: (password) => {
+        const isValid = password.length > 5 ? true : false
+        if (isValid) {
+            return [true, ""]
+        } else {
             return [false, "invalid password"]
         }
     },
-    repeatPass : (password, repeatPass) => {
-        if(password == repeatPass){
-            console.log("are matched")
+    repeatPass: (password, repeatPass) => {
+
+        if (password == repeatPass && password.length > 5) {
             return [true, ""]
-        }else{
+        } else {
             return [false, "passwords are not match!"]
         }
     }
