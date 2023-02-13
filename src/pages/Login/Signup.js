ceb0931fe86d9a5030c20e2b78e42245f94efc1e
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-13 15:22:08 -0800

    gender type selection added

--- src/pages/Login/Signup.js
+++ src/pages/Login/Signup.js
@@ -8,12 +8,13 @@ import { avatar } from "../../constants/UiConstant"
 
 function Signup() {
   const navigate = useNavigate();
-  const [profileImg, setProfileImg] = useState({ img: '', isOk: false });
+  const [profileImg, setProfileImg] = useState({ imgUrl: '', file: "", isOk: false });
   const [inputs, setInputs] = useState({
     name: { value: "", type: "text", placeholder: "name", isValid: false },
     lastName: { value: "", type: "text", placeholder: "last name", isValid: false },
     email: { value: "", type: "email", placeholder: "email", isValid: false },
     dob: { value: "", type: "date", placeholder: "birthday", isValid: false },
+    gender: { value: "", type: "select", placeholder: "gender", isValid: false },
     password: { value: "", type: "password", placeholder: "password", isValid: false },
     repeatPass: { value: "", type: "password", placeholder: "repeat password", isValid: false }
   });
@@ -36,6 +37,9 @@ function Signup() {
       case "dob":
         [copyState[name].isValid, copyState[name].warning] = validationFuncations.dobValidation(value)
         break;
+      case "gender":
+        [copyState[name].isValid, copyState[name].warning] = validationFuncations.genderValidation(value)
+        break;
       case "password":
         [copyState[name].isValid, copyState[name].warning] = validationFuncations.passwordValidation(value)
         break;
@@ -46,40 +50,78 @@ function Signup() {
     setInputs(copyState)
   }
 
+  // handle input change of profile image
   const setProfileImgInput = (e) => {
     console.log(e)
     const url = URL.createObjectURL(e.target.files[0])
     console.log(url)
-    setProfileImg({ img: url, isOk: true })
+    setProfileImg({ imgUrl: url, file: e.target.files[0], isOk: true })
   }
+
+  const sendInformationToServer = () => {
+    // check all inputs if there is any empty or invalid input
+    const invalids = Object.keys(inputs).filter(item => {
+      return inputs[item].isValid == false;
+    })
+    // if there is any empty or invalid input then alert a warning to user
+    if (invalids.length > 0) {
+      invalids.forEach(item => {
+        changeInputValue(item, inputs[item].value)
+      })
+      console.log(invalids)
+      return
+    }
+    console.log(inputs)
+
+
+  }
+
+
 
   console.log(inputs)
   return (
     <section className='signup fade_in'>
       <form className='display_flex flex_direction_column align_items_center'>
         <div className='input_profile_container position_relative '>
-          <img src={profileImg.isOk ? profileImg.img : avatar} className='input_profile_img' alt='user_image' />
+          <img src={profileImg.isOk ? profileImg.imgUrl : avatar} className='input_profile_img' alt='user_image' />
           <span className="upload_icon display_flex align_items_center justify_content_center">
             <i className="bi bi-camera"></i>
           </span>
           <input type={"file"} accept="image/*" className="input " onChange={(e) => setProfileImgInput(e)} />
         </div>
         {Object.keys(inputs).map(name => {
+          if (inputs[name].type == "select") {
+            return (
+              <>
+                <select
+                  className={`input select ${!inputs[name].isValid && inputs[name].isUsed ? "input_warning" : ""}`}
+                  value={inputs[name].value}
+                  defaultValue={"gender"}
+                  onChange={(e) => changeInputValue(name, e.target.value)}>
+                  <option disabled>gender</option>
+                  <option className='input'>Male</option>
+                  <option>Female</option>
+                </select>
+                {inputs[name].isValid ? null : <span className="warning">{inputs[name].warning}</span>}
+              </>
+            )
+          }
           return (
             <>
               <input
                 key={name}
                 type={inputs[name].type}
                 value={inputs[name].value}
-                name="name" 
-                className={`input ${!inputs[name].isValid && inputs[name].isUsed ? "input_warning": ""}`}
+                name="name"
+                className={`input ${!inputs[name].isValid && inputs[name].isUsed ? "input_warning" : ""}`}
                 placeholder={inputs[name].placeholder}
                 onChange={(e) => changeInputValue(name, e.target.value)} />
               {inputs[name].isValid ? null : <span className="warning">{inputs[name].warning}</span>}
             </>
           )
         })}
-        <Button name={"Sign up"} cursor="pointer" type={"general"} />
+
+        <Button name={"Sign up"} click={sendInformationToServer} cursor="pointer" type={"general"} />
         <div className="help_buttons display_flex justify_content_space_between">
           <Button name={"Already have account?"} cursor="pointer" click={() => navigate("/login")} />
         </div>
