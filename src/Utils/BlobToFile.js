db8e73fa0631f04b62e49f8a1c9b937770772595
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-15 01:47:51 -0800

    this utils has function to convert binary string into file

---
+++ src/Utils/BlobToFile.js
@@ -0,0 +1,27 @@
+export function BytesToFile(data, type) {
+    
+    if(data == "undefined" || !data){
+        return ""
+    }
+    let binaryString = window.atob(data);
+    let binaryLength = binaryString.length;
+    let bytes = new Uint8Array(binaryLength);
+    for (let i = 0; i < binaryLength; i++) {
+        let ascii = binaryString.charCodeAt(i);
+        bytes[i] = ascii;
+    }
+    let blob = new Blob([bytes], { type: type });
+    return window.URL.createObjectURL(blob);
+}
+
+export function getBlobOfFile(data, type){
+    let binaryString = window.atob(data);
+    let binaryLength = binaryString.length;
+    let bytes = new Uint8Array(binaryLength);
+    for (let i = 0; i < binaryLength; i++) {
+        let ascii = binaryString.charCodeAt(i);
+        bytes[i] = ascii;
+    }
+    return new Blob([bytes], { type: type });
+}
+ 
