029644945db64ca6e5f4d6e06da756ed81a201a3
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-14 18:08:35 -0800

    genderValidation function check value first if it isn't null

--- src/Utils/ValidationsFunctions.js
+++ src/Utils/ValidationsFunctions.js
@@ -27,7 +27,10 @@ export const validationFuncations = {
         }
     },
     genderValidation: (value) => {
-        value = value.toLowerCase()
+        if(value != null){
+            value = value.toLowerCase()
+        }
+            
         if (value == "male" || value == "female") {
             return [true, ""]
         } else {
