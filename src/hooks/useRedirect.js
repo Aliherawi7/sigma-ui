1d85006082ae43512b623c3192479fb897e23aee
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-15 01:48:38 -0800

    custome hooks

---
+++ src/hooks/useRedirect.js
@@ -0,0 +1,15 @@
+import React ,{useEffect} from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function useRedirect() {
+    const state = useSelector(state => state.authentication)
+    const navigate = useNavigate();
+    useEffect(() => {
+        if(!state.isAuthenticated){
+            navigate("/login")
+        }
+    }, [])
+}
+
+export default useRedirect;
