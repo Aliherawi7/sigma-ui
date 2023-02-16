61154f1e69cc7f1e2ddcd0d209b753eb3ef6fba7
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-15 23:01:10 -0800

    prfile compoente added to the routes

--- src/App.js
+++ src/App.js
@@ -5,35 +5,40 @@ import Spinner from './components/UI/Loading/Spinner';
 import { Suspense } from 'react';
 import Signup from './pages/Login/Signup';
 import { useSelector } from 'react-redux';
+import Wrapper from './components/HigherOrderComponent/Wrapper';
 
-const Home = React.lazy(() => import("./pages/Home/Home"))
-const Navbar = React.lazy(() => import('./components/navbar/Navbar'))
-const NotificationArea = React.lazy(() => import('./components/NotificationArea/NotificationArea'))
-const Chat = React.lazy(() => import('./pages/Chat/Chat'))
-const Login = React.lazy(() => import("./pages/Login/Login"))
+const Home = React.lazy(() => import("./pages/Home/Home"));
+const Navbar = React.lazy(() => import('./components/navbar/Navbar'));
+const NotificationArea = React.lazy(() => import('./components/NotificationArea/NotificationArea'));
+const Chat = React.lazy(() => import('./pages/Chat/Chat'));
+const Login = React.lazy(() => import("./pages/Login/Login"));
+const Profile = React.lazy(() => import("./pages/Profile/Profile"));
 
 function App() {
   const state = useSelector(state => state.authentication)
- 
+
   return (
-      <div className="App">
-        <main className='layout'>
-          <Router>
+    <div className="App">
+      <main className='layout'>
+        <Router>
           <Suspense fallback={<Spinner />}>
             {state.isAuthenticated ? <Navbar /> : null}
-            {state.isAuthenticated ? <NotificationArea /> : null }
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/chat' element={<Chat />} />
-              <Route path='load' element={<Spinner />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/signup' element={<Signup />} />
-              <Route path='*' element={<h2>Not Found</h2>} />
-            </Routes>
-            </Suspense>
-          </Router>
-        </main>
-      </div>
+            {state.isAuthenticated ? <NotificationArea /> : null}
+            <Wrapper>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/chat' element={<Chat />} />
+                <Route path='load' element={<Spinner />} />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/signup' element={<Signup />} />
+                <Route path='*' element={<h2>Not Found</h2>} />
+              </Routes>
+            </Wrapper>
+          </Suspense>
+        </Router>
+      </main>
+    </div>
   );
 }
 
