346676256108d20b5d14d63a271f9b9b31974e3e
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-18 15:30:53 -0800

    prfile account section moved to HeaderNavbar component

--- src/components/NotificationArea/NotificationArea.js
+++ src/components/NotificationArea/NotificationArea.js
@@ -10,26 +10,8 @@ function NotificationArea() {
   console.log(state);
   return (
     <section className='notification_area'>
-      <section className='header'>
-        <div className='search_bar'>
-          <i className={Icons.search}></i>
-          <form>
-            <input type="text" className='search_box input' placeholder='search' />
-          </form>
-        </div>
-        <section className='profile_info display_flex_align_center'>
-          <div className='notifications'>
-            <Button icon={Icons.notification} />
-          </div>
-          <div className='profile_account display_flex_align_center none_selectable'>
-            <img src={state.profileImage} className='profile_avatar_small' />
-            {/* <h3 className='name'>
-              {state.userName}
-            </h3> */}
-          </div>
-        </section>
-      </section>
-      <section className='requests section_splitor'>
+      
+      <section className='requests box_style section_splitor'>
         <div className='header_container display_flex_align_center'>
           <h3>Requests</h3>
           <span className='total_budge'>5</span>
@@ -47,7 +29,7 @@ function NotificationArea() {
 
 
       </section>
-      <section className='contacts section_splitor'>
+      <section className='contacts box_style section_splitor'>
         <div className='header_container display_flex_align_center'>
           <h3>Contacts</h3>
           <span className='total_budge'>54</span>
