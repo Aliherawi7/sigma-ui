3d48f423faf4ec8faa0040d4a266a8fb427be4db
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-17 17:56:59 -0800

    stories component created

---
+++ src/components/Stories/Stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Stories.css"
+
+function Stories() {
+    return (
+        <section className='stories'>
+            <div className='stories_container'>
+                <div className='story'>story</div>
+            </div>
+        </section>
+    )
+}
+
+export default Stories
