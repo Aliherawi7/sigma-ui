740f08f5a57953af23fc67e37048869f598f3a83
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-20 15:56:58 -0800

    code reformatted

--- src/components/UI/button/Button.js
+++ src/components/UI/button/Button.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./Button.css"
 
-function Button({name, type, icon, click, cursor = "unset"}) {
+function Button({ name, type, icon, click, cursor = "unset" }) {
   return (
-    <button className={`btn ${type}`} onClick={click} type="button" style={{cursor : cursor}}><i className={icon}></i> {name}</button>
+    <button
+      className={`btn ${type}`}
+      onClick={click}
+      type="button"
+      style={{ cursor: cursor }}>
+      <i className={icon}></i> {name}</button>
   )
 }
 
