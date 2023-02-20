efb03f11a27852333f46751f59a736ed5327fc28
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-19 18:41:08 -0800

    not found page created

---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import useRedirect from '../../hooks/useRedirect'
+import "./NotFound.css"
+
+function NotFound() {
+    useRedirect()
+    return (
+        <div className='not_found fade_in display_flex display_flex_align_center height_100 flex_direction_column justify_content_center'>
+            <h2>404</h2>
+            <p className='title'>Ooops... page not found</p>
+        </div>
+    )
+}
+
+export default NotFound
