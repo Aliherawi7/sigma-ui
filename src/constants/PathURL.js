149fccbeaa4b17144a150c4d6623bdd225fdc23b
Aliherawi7 <Aliherawi7@gmail.com>
2023-02-26 23:17:07 -0800

    new urls added

--- src/constants/PathURL.js
+++ src/constants/PathURL.js
@@ -10,5 +10,10 @@ export const Paths = {
 
 export const APIEndpoints = {
     PEOPLE: "http://localhost:9090/api/accounts/all",
-    ONE_PERSON:"http://localhost:9090/api/accounts/userName/"
+    ONE_PERSON:"http://localhost:9090/api/accounts/userName/",
+    POST:"http://localhost:9090/api/posts/userName/",
+    ADD_FRIEND_REQUEST: 'http://localhost:9090/api/friendRequests',
+    ALL_Received_Friend_Reqeuests:"http://localhost:9090/api/friendRequests/allReceived",
+    ACCEPT_FRIEND_REQUEST:"http://localhost:9090/api/friendRequests/accept",
+    ALL_Friends : (userName) => `http://localhost:9090/api/accounts/${userName}/friends`,
 }
