c4c23831802f9e385d0c10c2b208b4c2fd068396
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-02 17:20:50 -0800

    test message updated

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -7,7 +7,7 @@ function NotFound() {
     return (
         <div className='not_found fade_in display_flex display_flex_align_center height_100 flex_direction_column justify_content_center'>
             <h2>404</h2>
-            <p className='title'>Ooops... page not found</p>
+            <p className='title'>Ooops... resource not found</p>
         </div>
     )
 }
