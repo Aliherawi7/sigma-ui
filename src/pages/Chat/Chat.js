5238eface7b00608180fcb071aeeacb3ba5562a6
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-16 15:59:20 +0430

    contacts list seprated from chat component, unused styles removed

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -1,72 +1,40 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Button from '../../components/UI/button/Button'
 import { Icons } from '../../constants/UiConstant'
 import "./Chat.css"
 import useRedirect from '../../hooks/useRedirect'
+import { APIEndpoints } from '../../constants/PathURL'
+import useFetch from "../../hooks/useFetch"
+import { useSelector } from 'react-redux'
+import ContactsList from '../../components/ContanctsList/ContactsList'
+
+
 
 
 function Chat() {
     useRedirect();
+    const auth = useSelector(state => state.authentication)
+    
+    const { data, error, loading, setData } =
+    useFetch(APIEndpoints.MESSAGE_OF_SPECIFIC_ACCOUNT("userName"), { method: "GET", headers: { "Authorization": auth.token } })
+    
+    useEffect(() => {
+        
+        
+    }, [])
+
+
+    const onMessageReceive = (msg) => {
+        
+    }
+    
+    const sendMessage = (msg) => {
+        
+    }
+    console.log(data)
     return (
         <section className='chat height_100 fade_in '>
-            <div className='contacts_list'>
-                <div className='search_bar'>
-                    <i className={Icons.search}></i>
-                    <form>
-                        <input type="text" className='search_box input' placeholder='search in your inbox' />
-                    </form>
-                </div>
-                <div className='contacts_container'>
-                    <div className='contact_to_chat display_flex_align_center click_effect'>
-                        <div className='contact_info_container display_flex_align_center'>
-                            <div className='profile_img_container display_flex position_relative'>
-                                <img src='\images\ProfileImages\user3.jpg' className='profile_avatar_small' />
-                                <span className='active_badge'></span>
-                            </div>
-                            <div className='contact_text'>
-                                <h5>user name</h5>
-                                <p className='last_message'>hi dear friend how are you</p>
-                            </div>
-                        </div>
-                        <div className='badge_container display_flex_align_center flex_direction_column'>
-                            <span className='last_seen'>just now</span>
-                            <span className='badge new_message_counter'>1</span>
-                        </div>
-                    </div>
-                    <div className='contact_to_chat display_flex_align_center click_effect'>
-                        <div className='contact_info_container display_flex_align_center'>
-                            <div className='profile_img_container display_flex position_relative'>
-                                <img src='\images\ProfileImages\user1.jpg' className='profile_avatar_small' />
-                                <span className='active_badge'></span>
-                            </div>
-                            <div className='contact_text'>
-                                <h5>user name</h5>
-                                <p className='last_message'>hi dear friend how are you</p>
-                            </div>
-                        </div>
-                        <div className='badge_container display_flex_align_center flex_direction_column'>
-                            <span className='last_seen'>just now</span>
-                            <span className='badge new_message_counter'>5</span>
-                        </div>
-                    </div>
-                    <div className='contact_to_chat display_flex_align_center click_effect'>
-                        <div className='contact_info_container display_flex_align_center'>
-                            <div className='profile_img_container display_flex position_relative'>
-                                <img src='\images\ProfileImages\user2.jpg' className='profile_avatar_small' />
-                                <span className='active_badge'></span>
-                            </div>
-                            <div className='contact_text'>
-                                <h5>user name</h5>
-                                <p className='last_message'>hi dear friend how are you</p>
-                            </div>
-                        </div>
-                        <div className='badge_container display_flex_align_center flex_direction_column'>
-                            <span className='last_seen'>just now</span>
-                            <span className='badge new_message_counter'>2</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            {<ContactsList />}
             <div className='messages_panel'>
                 <div className='header display_flex'>
                     <img src='\images\ProfileImages\user2.jpg' className='profile_avatar_small' />
@@ -95,11 +63,11 @@ function Chat() {
                                 <span className='message_date'>10:20 am</span>
                             </div>
                         </div>
-                        
+
                     </div>
                     <div className='input_panel'>
                         <Button icon={Icons.emojis.smile} />
-                        <input type={'text'} className='input width_100' placeholder='Type a message'/>
+                        <input type={'text'} className='input width_100' placeholder='Type a message' />
                         <Button icon={Icons.send} />
                     </div>
                 </div>
