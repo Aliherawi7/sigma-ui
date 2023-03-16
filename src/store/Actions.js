1d0c4db20dafb6783fa788b654138d119d8afea2
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-16 23:11:04 +0430

    add currentChat action added to actions

--- src/store/Actions.js
+++ src/store/Actions.js
@@ -1,7 +1,8 @@
 const actions = {
     ADD_USER_INFO : 'add_user_info',
     ADD_CRURRENT_URL: "add_current_url",
-    LOGOUT:"logout"
+    LOGOUT:"logout",
+    CURRENT_CHAT:"current_chat"
 }
 
 export default actions;
