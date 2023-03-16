f1c0aa7f5e78df1f53a659b3657d9bd10c0bfc76
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-16 23:13:33 +0430

    current chat user info is added to the chat box

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -14,33 +14,53 @@ import ContactsList from '../../components/ContanctsList/ContactsList'
 function Chat() {
     useRedirect();
     const auth = useSelector(state => state.authentication)
-    
+    let currentChat = useSelector(state => state.currentChat);
+    const [currentFrient, setCurrentFriend] = useState({});
+
     const { data, error, loading, setData } =
-    useFetch(APIEndpoints.MESSAGE_OF_SPECIFIC_ACCOUNT("userName"), { method: "GET", headers: { "Authorization": auth.token } })
-    
+        useFetch(APIEndpoints.MESSAGE_OF_SPECIFIC_ACCOUNT(currentChat ? currentChat : ""),
+            { method: "GET", headers: { "Authorization": auth.token } })
+
     useEffect(() => {
-        
-        
+        fetch(APIEndpoints.ONE_PERSON+currentChat,{
+            method:"GET",
+            headers:{
+                "Authorization":auth.token,
+                "content-type":"application/json"
+            },
+            
+        })
+        .then(res => {
+            if(res.ok){
+                return res.json();
+            }else{
+                throw new Error(res.statusText);
+            }
+        }).then(data => {
+            console.log(data)
+            setCurrentFriend(data)
+        })
+
     }, [])
 
 
     const onMessageReceive = (msg) => {
-        
+
     }
-    
+
     const sendMessage = (msg) => {
         
     }
-    console.log(data)
+    console.log(data,"in chat log")
     return (
         <section className='chat height_100 fade_in '>
             {<ContactsList />}
             <div className='messages_panel'>
                 <div className='header display_flex'>
-                    <img src='\images\ProfileImages\user2.jpg' className='profile_avatar_small' />
+                    <img src={APIEndpoints.HOSTNAME+currentFrient.profilePictureUrl} className='profile_avatar_small' />
                     <div className='contact_name_status'>
                         <h5 className='name'>
-                            Maria Johns
+                            {currentFrient.name + " " + currentFrient.lastName}
                             <span className='active_badge'> </span>
                         </h5>
                         <p className='last_seen'>Active now</p>
@@ -49,20 +69,33 @@ function Chat() {
                 </div>
                 <div className='message_panel'>
                     <div className='messages_container display_flex flex_direction_column'>
-                        <div className='send_message display_flex'>
-                            <div className='message_body display_flex flex_direction_column'>
-                                <p className='message_value'>Hi guys this is a test message</p>
-                                <span className='message_date'>10:20 am</span>
-                            </div>
-                            <img src='\images\ProfileImages\user3.jpg' className='profile_avatar_small' />
-                        </div>
-                        <div className='receive_message display_flex'>
-                            <img src='\images\ProfileImages\user2.jpg' className='profile_avatar_small' />
-                            <div className='message_body display_flex flex_direction_column'>
-                                <p className='message_value'>Hi guys this is a test message I made this line's width a little longer to test the size of message body</p>
-                                <span className='message_date'>10:20 am</span>
-                            </div>
-                        </div>
+                        {data?.map(item => {
+                            var date = new Date(item.dateTime)
+                        
+                            if (item.receiverUsername == auth.userName) {
+                                return (
+                                    <div className='receive_message display_flex'>
+                                        <img src={APIEndpoints.HOSTNAME+item.senderProfileImageUrl} className='profile_avatar_small' />
+                                        <div className='message_body display_flex flex_direction_column'>
+                                            <p className='message_value'>{item.text}</p>
+                                            <span className='message_date'>{date.getHours()+":"+date.getMinutes()}</span>
+                                        </div>
+                                    </div>
+                                )
+                            }else {
+                                return(
+                                <div className='send_message display_flex'>
+                                    <div className='message_body display_flex flex_direction_column'>
+                                        <p className='message_value'>{item.text}</p>
+                                        <span className='message_date'>{date.getHours()+":"+date.getMinutes()}</span>
+                                    </div>
+                                    <img src={APIEndpoints.HOSTNAME+item.senderProfileImageUrl} className='profile_avatar_small' />
+                                </div>
+                                )
+                            }
+                        })}
+
+
 
                     </div>
                     <div className='input_panel'>
