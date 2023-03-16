d392e3219a5090e6034c98a17d40ff9c406944b7
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-16 23:12:19 +0430

    onClick event listener added

--- src/components/ContanctsList/ContactsList.js
+++ src/components/ContanctsList/ContactsList.js
@@ -1,31 +1,37 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { APIEndpoints } from '../../constants/PathURL';
 import { Icons } from '../../constants/UiConstant';
 import useFetch from '../../hooks/useFetch';
+import actions from '../../store/Actions';
 import Error from '../UI/Error/Error';
 import Spinner from '../UI/Loading/Spinner';
 import "./ContactsList.css";
 
 
-
 function ContactsList() {
     const auth = useSelector(state => state.authentication)
-    console.log(auth)
+    const dispatch = useDispatch();
     const { data, error, loading, setData } =
         useFetch(APIEndpoints.ALL_FRIENDS(auth.userName), { method: "GET", headers: { "Authorization": auth.token } });
+    const currentChat = (username) => {
+        dispatch({
+            type:actions.CURRENT_CHAT,
+            payload: username
+        })
+    }
     let element;
     if (loading) {
         element = <Spinner />;
     }else if(error){
         element = <Error />
-        
     }else if(data){
         element = (
             data?.map(item => {
-                console.log(item , "in log")
                 return (
-                    <div className='contact_to_chat display_flex_align_center click_effect'>
+                    <div className='contact_to_chat display_flex_align_center click_effect' 
+                    onClick={() => currentChat(item.userName)}
+                    key={item.userName}>
                         <div className='contact_info_container display_flex_align_center'>
                             <div className='profile_img_container display_flex position_relative'>
                                 <img src={APIEndpoints.HOSTNAME+item.profilePictureUrl} className='profile_avatar_small' />
