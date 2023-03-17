8f4cf6c385aeee2a5d347cc7a3c0b6cc92bfcc50
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-17 18:12:42 +0430

    add people and add currentChat added to the actions

--- src/store/Actions.js
+++ src/store/Actions.js
@@ -2,7 +2,8 @@ const actions = {
     ADD_USER_INFO : 'add_user_info',
     ADD_CRURRENT_URL: "add_current_url",
     LOGOUT:"logout",
-    CURRENT_CHAT:"current_chat"
+    CURRENT_CHAT:"current_chat",
+    ADD_PEOPLE:"add_people"
 }
 
 export default actions;
