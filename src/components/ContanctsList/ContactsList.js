912e1b2b4bc8d1592bee202ef26dfbdd8c5e6eb8
Aliherawi7 <Aliherawi7@gmail.com>
2023-03-17 18:07:43 +0430

    this component is using pagination to fetch the list of friends and update the currentchat person while clicking on the contact in the list

--- src/components/ContanctsList/ContactsList.js
+++ src/components/ContanctsList/ContactsList.js
@@ -4,53 +4,63 @@ import { APIEndpoints } from '../../constants/PathURL';
 import { Icons } from '../../constants/UiConstant';
 import useFetch from '../../hooks/useFetch';
 import actions from '../../store/Actions';
+import { setCookie } from '../../Utils/Cookie';
 import Error from '../UI/Error/Error';
 import Spinner from '../UI/Loading/Spinner';
 import "./ContactsList.css";
 
 
-function ContactsList() {
+function ContactsList({setCurrentChat}) {
     const auth = useSelector(state => state.authentication)
     const dispatch = useDispatch();
     const { data, error, loading, setData } =
-        useFetch(APIEndpoints.ALL_FRIENDS(auth.userName), { method: "GET", headers: { "Authorization": auth.token } });
+        useFetch(APIEndpoints.ALL_FRIENDS(auth.userName, 1, 15), { method: "GET", headers: { "Authorization": auth.token } });
     const currentChat = (username) => {
+        setCookie("lastChat", username, 20)
         dispatch({
-            type:actions.CURRENT_CHAT,
+            type: actions.CURRENT_CHAT,
             payload: username
         })
+        setCurrentChat(username)
+
     }
+
+
     let element;
     if (loading) {
         element = <Spinner />;
-    }else if(error){
+    } else if (error) {
         element = <Error />
-    }else if(data){
+    } else if (!data) {
+        element = "You have no friend"
+    } else if (data) {
+
         element = (
-            data?.map(item => {
-                return (
-                    <div className='contact_to_chat display_flex_align_center click_effect' 
-                    onClick={() => currentChat(item.userName)}
-                    key={item.userName}>
-                        <div className='contact_info_container display_flex_align_center'>
-                            <div className='profile_img_container display_flex position_relative'>
-                                <img src={APIEndpoints.HOSTNAME+item.profilePictureUrl} className='profile_avatar_small' />
-                                <span className='active_badge'></span>
+            data.length > 0 ?
+                data?.map(item => {
+                    return (
+                        <div className='contact_to_chat display_flex_align_center click_effect'
+                            onClick={() => currentChat(item.userName)}
+                            key={item.userName}>
+                            <div className='contact_info_container display_flex_align_center'>
+                                <div className='profile_img_container display_flex position_relative'>
+                                    <img src={APIEndpoints.HOSTNAME + item.profilePictureUrl} className='profile_avatar_small' />
+                                    <span className='active_badge'></span>
+                                </div>
+                                <div className='contact_text'>
+                                    <h5>{item.name + " " + item.lastName}</h5>
+                                    <p className='last_message'>hi dear friend how are you</p>
+                                </div>
                             </div>
-                            <div className='contact_text'>
-                                <h5>{item.name +" "+ item.lastName}</h5>
-                                <p className='last_message'>hi dear friend how are you</p>
+                            <div className='badge_container display_flex_align_center flex_direction_column'>
+                                <span className='last_seen'>just now</span>
+                                <span className='badge new_message_counter'>1</span>
                             </div>
                         </div>
-                        <div className='badge_container display_flex_align_center flex_direction_column'>
-                            <span className='last_seen'>just now</span>
-                            <span className='badge new_message_counter'>1</span>
-                        </div>
-                    </div>
-                )
-            })
-        );
-    }    
+                    )
+                })
+                : <h5>You don't have any friend</h5>);
+    }
     return (
         <div className='contacts_list'>
             <div className='search_bar'>
